f18f5944a1add5141c75314da1ee7637cc0a41fa
Gavin Staniforth <gsdevme@users.noreply.github.com>
2017-05-12 23:00:20 +0100

    chore: more init stuff

--- web/index.php
+++ web/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'test';
+require __DIR__.'/../vendor/autoload.php';
+
+use Symfony\Component\VarDumper\VarDumper;
+
+$mongo = new \MongoDB\Driver\Manager('mongodb://127.0.0.1/');
+
+
+VarDumper::dump($mongo);
